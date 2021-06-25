708a10e0bea3a716d948a5d5d3e0c90d8ba01a4e
Mark Lescroart <mark.lescroart@gmail.com>
2021-06-24 23:25:03 -0700

    edited environment setup, stub of file loading

--- set_up_environment.sh
+++ set_up_environment.sh
@@ -1,23 +1,44 @@
 # Environment setup
 
-export name=summer_workshop_test
+export name=vedb_workshop_test
 
 # Create initial environment
-mamba env create -f environment.yml
+#mamba env create -f environment.yml
 
 # Activate it
+#conda init bash
 conda activate $name
+
+# Make source code directory for git libraries
+if [ -d ~/Code ]
+	then
+		:
+	else
+	mkdir ~/Code
+fi
+cd ~/Code/
 
 # Retrieve and install libraries from git...
 # ... for loading files:
-git clone git@github.com:vedb/file_io.git
+if [ -d file_io ]
+then
+	:
+else
+	git clone git@github.com:vedb/file_io.git
+fi
 cd file_io
 python setup.py install
 cd ..
 # ... for plotting:
-git clone git@github.com:piecesofmindlab/plot_utils.git
+if [ -d plot_utils ]
+then
+	:
+else
+	git clone git@github.com:piecesofmindlab/plot_utils.git
+fi
 cd plot_utils
 python setup.py install
+cd ..
 cd ..
 
 ipython kernel install --user --name $name
